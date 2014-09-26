bd427e33a42757c853b668fb6ece76ca4d055036
adriano07 <abreoadrian@gmail.com>
2014-09-26 16:34:41 -0500

    segundo archivo subido

---
+++ prueba/practicas/act2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practicas;
+
+/**
+ *
+ * @author DOCENTE
+ */
+public class act2 {
+    public static void main( String args[] )
+{
+    int x=5, y=2, resi;
+    resi = x/y;
+    double resd;
+    resd = (double) x/y;
+    System.out.println( "resi: "+resi );
+    System.out.println( "resd: "+resd );
+    System.out.println("Esto es una prueba");
+} 
+}
